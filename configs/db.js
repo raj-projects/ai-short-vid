09c7b82eb4ad8a102bc0a3d84285d6ab815fe74e
raj-projects <niit.rahulraj@gmail.com>
2025-07-23 14:26:03 +0530

    Save new user in DB

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,5 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL);
-const db = drizzle({ client: sql });
+export const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL);
+export const db = drizzle({ client: sql });
