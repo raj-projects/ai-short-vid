e74e3c0d5d8db3fec9ed750afa5c51292ef9726b
raj-projects <niit.rahulraj@gmail.com>
2025-07-24 11:03:04 +0530

    Updated select content field with functionality

---
+++ app/dashboard/_components/_components/SelectTopics.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+
+const SelectTopics = ({ onUserSelect }) => {
+  const options = [
+    { value: "custom-prompt", label: "Custom Prompt" },
+    { value: "random-ai-story", label: "Random AI Story" },
+    { value: "scary-story", label: "Scary Story" },
+    { value: "historycal-facts", label: "Historycal Facts" },
+    { value: "bad-time-story", label: "Bad Time Story" },
+    { value: "motivational-story", label: "Motivational Story" },
+    { value: "fun-facts", label: "Fun Facts" },
+  ];
+  const [selectedOption, setSelectedOption] = React.useState("");
+
+  return (
+    <div>
+      <h2 className="font-bold text-2xl text-primary">Content</h2>
+      <p>What is the topic of your video?</p>
+
+      <Select
+        onValueChange={(value) => {
+          setSelectedOption(value);
+          value !== "custom-prompt" && onUserSelect('topic', value);
+        }}
+      >
+        <SelectTrigger className="w-full mt-2 p-6 text-lg">
+          <SelectValue placeholder="Content Type" />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((option) => (
+            <SelectItem key={option.value} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      {selectedOption === "custom-prompt" && (
+        <Textarea
+          className={"mt-3"}
+            onChange={(e) => onUserSelect('topic', e.target.value)}
+          placeholder="Write a prompt on which you want to generate"
+        />
+      )}
+    </div>
+  );
+};
+
+export default SelectTopics;
--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,10 +1,30 @@
+"use client";
 import React from "react";
+import SelectTopics from "../_components/_components/SelectTopics";
 
 const CreateNew = () => {
+  const [formData, setFormData] = React.useState([]);
+  const onHandleInputChange = (fieldName, fileldValue) => {
+    console.log(fieldName, fileldValue);
+  };
+
   return (
-    <>
-      <h2 className="font-bold text-2xl text-primary">Create new</h2>
-    </>
+    <div className="md:px-20">
+      <h2 className="font-bold text-2xl text-primary text-center">
+        Create new
+      </h2>
+
+      <div className="mt-10 shadow-sm p-10">
+        {/* Select Topic */}
+        <SelectTopics onUserSelect={onHandleInputChange} />
+
+        {/* Select Style */}
+
+        {/* Select Duration */}
+
+        {/* Generate */}
+      </div>
+    </div>
   );
 };
 
